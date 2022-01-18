fc749ee575aa9ce022101aeaaa6576a0aefe6462
David Bennett <hexadecagon@gmail.com>
2022-01-17 20:51:24 -0600

    lesson 20

--- main.go
+++ main.go
@@ -16,27 +16,39 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func pathHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, r.URL.Path)
-	fmt.Fprintln(w, r.URL.RawPath)
-	// switch r.URL.Path {
-	// case "/":
-	// 	homeHandler(w, r)
-	// case "/contact":
-	// 	contactHandler(w, r)
-	// default:
-	// 	//handle page not found error
-	// 	//w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	// 	//fmt.Fprint(w, "<h1>INVALID URL</h>")
-	// 	w.WriteHeader(404)
-	// }
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		//handle page not found error
+		//w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		//fmt.Fprint(w, "<h1>INVALID URL</h>")
+		w.WriteHeader(404)
+	}
 }
 
+// type Router struct{}
+
+// func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// 	switch r.URL.Path {
+// 	case "/":
+// 		homeHandler(w, r)
+// 	case "/contact":
+// 		contactHandler(w, r)
+// 	default:
+// 		//handle page not found error
+// 		//w.Header().Set("Content-Type", "text/html; charset=utf-8")
+// 		//fmt.Fprint(w, "<h1>INVALID URL</h>")
+// 		w.WriteHeader(404)
+// 	}
+// }
+
 func main() {
-	http.HandleFunc("/", pathHandler)
-	//http.HandleFunc("/contact", contactHandler)
-	//time.Sleep(3 * time.Second)
-	//if you want to make sure your server is actually started
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting the server on :3000...")
 	//print isnt necessary, just a dev helper tool to make sure your code is running at least okayish
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", http.HandlerFunc(pathHandler))
 }
