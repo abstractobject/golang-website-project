002f2bd35b5fbfdac0d37588aa6b53d2beccafc2
David Bennett <hexadecagon@gmail.com>
2022-01-17 21:04:26 -0600

    lesson 21

--- main.go
+++ main.go
@@ -15,23 +15,7 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in contact with me, email at <a href=\"mailto:hexadecagon@gmail.com\">hexadecagon@gmail.com</a>")
 }
 
-func pathHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	default:
-		//handle page not found error
-		//w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		//fmt.Fprint(w, "<h1>INVALID URL</h>")
-		w.WriteHeader(404)
-	}
-}
-
-// type Router struct{}
-
-// func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// func pathHandler(w http.ResponseWriter, r *http.Request) {
 // 	switch r.URL.Path {
 // 	case "/":
 // 		homeHandler(w, r)
@@ -45,10 +29,25 @@ func pathHandler(w http.ResponseWriter, r *http.Request) {
 // 	}
 // }
 
+type Router struct{}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		//handle page not found error
+		//w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		//fmt.Fprint(w, "<h1>INVALID URL</h>")
+		w.WriteHeader(404)
+	}
+}
+
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/contact", contactHandler)
+	var router Router
 	fmt.Println("Starting the server on :3000...")
 	//print isnt necessary, just a dev helper tool to make sure your code is running at least okayish
-	http.ListenAndServe(":3000", http.HandlerFunc(pathHandler))
+	http.ListenAndServe(":3000", router)
 }
