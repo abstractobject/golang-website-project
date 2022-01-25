ff810053a8238f227f4718e12edc5a51402d74dc
David Bennett <hexadecagon@gmail.com>
2022-01-24 22:07:12 -0600

    trying chi middleware

--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
+	time.Sleep(11 * time.Second) //remove after testing timeout
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h>")
 }
@@ -64,6 +67,8 @@ func fourohfourHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
 	r.Get("/", homeHandler)
 	r.Get("/contact", contactHandler)
 	r.Get("/faq", faqHandler)
